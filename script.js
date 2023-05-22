611e7738294559fe03a4e385b1f87f9af282deba
manuelmartad <45702573+manuelmartad@users.noreply.github.com>
2023-05-22 16:42:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+const formulario = document.querySelector("#formulario");
+const curp = document.querySelector("#curp");
+
+function validarCampos(event) {
+    event.preventDefault();
+
+    // Validate the curp field is not empty and trim spaces
+    if (curp.value.trim() === "") {
+        showAlert("Por favor, ingresa tu CURP.");
+        return;
+    }
+
+    // Check that string length is 18 characters
+    if (curp.value.trim().length !== 18) {
+        showAlert("La longitud de tu CURP debe ser 18 caracteres.");
+        return;
+    }
+
+    const cadena = curp.value.trim();
+    const [primeraLetra, segundaLetra, tercerLetra, cuartaLetra] = cadena;
+
+    const esLetra = /^[A-Za-z]$/.test(primeraLetra);
+    const esVocal = /[aeiouáéíóú]/i.test(segundaLetra);
+    const esTercerLetra = /^[A-Za-z]$/.test(tercerLetra);
+    const esCuartaLetra = /^[A-Za-z]$/.test(cuartaLetra);
+    const fechaNacimiento = cadena.substr(4, 6);
+    const esFecha = /^\d{6}$/.test(fechaNacimiento);
+    const sexo = cadena.charAt(10);
+    const esSexo = /^[HM]$/i.test(sexo);
+    const estado = cadena.substr(11, 2);
+    const esEstado = /^[A-Za-z]{2}$/.test(estado);
+
+    if (!esLetra || !esVocal || !esTercerLetra || !esCuartaLetra || !esFecha || !esSexo || !esEstado) {
+        showAlert("Por favor, verifica los campos de tu CURP.");
+        return;
+    }
+
+    formulario.submit();
+}
+
+curp.addEventListener("blur", (event) => {
+    const cadena = event.target.value;
+    const [primeraLetra, segundaLetra, tercerLetra, cuartaLetra] = cadena;
+
+    const esLetra = /^[A-Za-z]$/.test(primeraLetra);
+    const esVocal = /[aeiouáéíóú]/i.test(segundaLetra);
+    const esTercerLetra = /^[A-Za-z]$/.test(tercerLetra);
+    const esCuartaLetra = /^[A-Za-z]$/.test(cuartaLetra);
+    const fechaNacimiento = cadena.substr(4, 6); // Extract the date of birth substring
+    const esFecha = /^\d{6}$/.test(fechaNacimiento);
+    const sexo = cadena.charAt(10);
+    const esSexo = /^[HM]$/i.test(sexo);
+    const estado = cadena.substr(11, 2); // Extract the estado substring
+    const esEstado = /^[A-Za-z]{2}$/.test(estado); // Validate the estado format
+
+    if (!esLetra) {
+        showAlert("El primer caracter de tu CURP debe ser la primer letra de tu primer apellido.");
+        return;
+    }
+    if (!esVocal) {
+        showAlert("La segunda letra de tu CURP debe ser la primer vocal interna de tu primer apellido.");
+        return;
+    }
+    if (!esTercerLetra) {
+        showAlert("La tercera letra de tu CURP debe ser la primer letra del segundo apellido.");
+        return;
+    }
+    if (!esCuartaLetra) {
+        showAlert("La cuarta letra de tu CURP debe ser la primer letra de tu primer nombre.");
+        return;
+    }
+    if (!esFecha) {
+        showAlert("Los caracteres 5 al 10 de tu CURP deben representar tu fecha de nacimiento en formato Año, Mes y Día. Ejemplo: 920528.");
+        return;
+    }
+    if (!esSexo) {
+        showAlert("El onceavo caracter de tu CURP debe ser 'H' o 'M'.");
+        return;
+    }
+    if (!esEstado) {
+        showAlert("Los caracteres 12 y 13 de tu CURP deben representar las dos letras del lugar de nacimiento de acuerdo al código de la Entidad Federativa. Ejemplo: CH para el estado de Chihuahua");
+        return;
+    }
+});
+
+
+let alertBox = null;
+
+function showAlert(message) {
+    if (alertBox) {
+        // If an alert box already exists, update the message
+        alertBox.textContent = message;
+    } else {
+        // Create a new alert box
+        alertBox = document.createElement("div");
+        alertBox.classList.add("alert", "bg-danger", "text-light", "mt-5", "w-50", "mx-auto");
+        alertBox.textContent = message;
+        document.querySelector("h1").before(alertBox);
+    }
+
+    setTimeout(() => {
+        if (alertBox) {
+            // Remove the alert box only if it still exists
+            alertBox.remove();
+            alertBox = null;
+        }
+    }, 3000);
+}
+
+
+
+
+// Event listener for form submission
+formulario.addEventListener("submit", validarCampos);
+
+
